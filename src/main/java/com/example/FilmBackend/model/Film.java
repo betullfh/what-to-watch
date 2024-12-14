62bb0f7c9b8ec508f208c7d20f2b381cb5d465f9
betulturan <betul16turan@gmail.com>
2024-12-15 01:46:05 +0300

    Add Film model with attributes

---
+++ src/main/java/com/example/FilmBackend/model/Film.java
@@ -0,0 +1,74 @@
+package com.example.FilmBackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String genre;
+    private int release_year; // Güncellendi
+    private String director;
+    private double rating;
+    private String image_url;
+
+    // Getter ve Setter metotları
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getRelease_year() { // Güncellendi
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) { // Güncellendi
+        this.release_year = release_year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getImageUrl() {
+        return image_url;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.image_url = image_url;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
