0d88930a7a6d97a2c82b4d3db3c0b16f99dbc399
betulturan <betul16turan@gmail.com>
2024-12-15 02:00:41 +0300

    Add remaining project resources and configurations

---
+++ src/test/java/com/example/FilmBackend/FilmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FilmBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
