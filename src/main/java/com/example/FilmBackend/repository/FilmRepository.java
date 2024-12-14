85e89efb96135b4cd9afde4b9a89f2cc5dc6699f
betulturan <betul16turan@gmail.com>
2024-12-15 01:47:52 +0300

    Add FilmRepository for database interactions

---
+++ src/main/java/com/example/FilmBackend/repository/FilmRepository.java
@@ -0,0 +1,9 @@
+package com.example.FilmBackend.repository;
+
+import com.example.FilmBackend.model.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+}
