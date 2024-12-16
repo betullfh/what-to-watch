fd6b64d1ed04a3beba1a787c4439aa31e5d8eb0a
betulturan <betul16turan@gmail.com>
2024-12-16 12:49:52 +0300

    added navbar and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import Navbar from './components/Navbar';
 import RouterConfig from './config/RouterConfig'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import HomePage from './pages/HomePage';
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
 
   return (
   <div>
+    <Navbar/>
+    <HomePage/>
     <RouterConfig/>
     <ToastContainer />
    
